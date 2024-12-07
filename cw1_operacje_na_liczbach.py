d700b72bae3b4c4d8f960e492db3f80ae047877a
Emi <emi.wp.pl>
2024-12-07 13:49:30 +0100

    jakas wiadomosc

---
+++ cw1_operacje_na_liczbach.py
@@ -0,0 +1,12 @@
+# dane od uzytkownika
+number1 = float(input("Podaj pierwsza liczbe"))
+number2 = float(input("Podaj druga liczbe"))
+
+# operacje matematyczne
+add = number1 + number2
+
+potegowanie = number1 ** number2
+
+# wyswietlenie wyniku
+print(f"Suma Twoich liczb {number1}, {number2} to: {add}")
+print(f"{number1} do potegi  {number2} to: {potegowanie}")
